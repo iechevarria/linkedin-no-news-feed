35a6662bea7a3330dbc888e22a6c226a33e897b2
Ivan Echevarria <iechevarria@email.wm.edu>
2018-08-21 01:25:21 -0400

    Add background.js
    - Add background.js and ability to remove news feed on navigation
    - Add listener to linkedin-no-news-feed.js to remove feed on background.js message
    - Update manifest.json

---
+++ background.js
@@ -0,0 +1,12 @@
+// post: https://stackoverflow.com/questions/34957319/how-to-listen-for-url-change-with-chrome-extension
+// user: https://stackoverflow.com/users/6586407/ztrat4dkyle 
+browser.tabs.onUpdated.addListener(
+  function(tabId, changeInfo) {
+    if (changeInfo.url && changeInfo.url == 'https://www.linkedin.com/feed/') {
+      chrome.tabs.sendMessage( tabId, {
+        remove: true
+      })
+    }
+  }
+);
+  
--- linkedin-no-news-feed.js
+++ linkedin-no-news-feed.js
@@ -1,11 +1,16 @@
-let timer = setInterval(hideNewsFeed, 300)
-
-function hideNewsFeed() {
-  console.log('Searching for news feed')
-  if (document.getElementsByClassName('core-rail').length) {
-    document.getElementsByClassName('core-rail')[0].innerHTML = ''
-    console.log('News feed deleted')
-    clearInterval(timer)
-    return
-  }
+function hideNewsFeed () {
+  timer = setInterval(function deleteCoreRail() {
+    if (document.getElementsByClassName('core-rail').length) {
+      document.getElementsByClassName('core-rail')[0].innerHTML = ''
+      clearInterval(timer)
+    }
+  }, 100)
 }
+
+browser.runtime.onMessage.addListener(
+  function(request) {
+    if (request.remove) {
+      hideNewsFeed()
+    }
+  }
+);
